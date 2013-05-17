a0687eec14aa7f08513c9203b150b854a003cdb7
Adam Sunderland <iterion@gmail.com>
2013-05-17 17:03:30 -0500

    starting rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wildcard::Application.config.secret_token = '6a09f9e5fb57cfb1378a320ee151ed1db92308c09f0745d6d62a12fd6fe1268990308b81ca37f226aba7b84ec5a136ee1f891db8bb6b24250c59e1f958da48d9'
