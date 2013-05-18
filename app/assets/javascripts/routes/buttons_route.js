495557054054b909aff0af2553e2bdd64393dba6
Adam Sunderland <iterion@gmail.com>
2013-05-18 17:16:22 -0500

    adding button view

--- app/assets/javascripts/routes/buttons_route.js
+++ app/assets/javascripts/routes/buttons_route.js
@@ -1,8 +1,11 @@
 Wildcard.ButtonsRoute = Ember.Route.extend({
   model: function() {
     var buttons = [
-      Em.Object.create({url: 'images/douchebag.jpg', name: "A*hole"}),
-      Em.Object.create({url: 'images/test.jpg', name: "Pothole"})
+      Em.Object.create({url: 'assets/pothole2.jpg', name: "Pothole"}),
+      Em.Object.create({url: 'assets/douchebag.jpg', name: "A*hole"}),
+      Em.Object.create({url: 'assets/dogfriendly.jpg', name: "Doggie*Friendly"}),
+      Em.Object.create({url: 'assets/ticket.jpg', name: "Parking*Tickets"}),
+      Em.Object.create({url: 'assets/valet2.jpg', name: "Valet"})
     ];
     // var ap = Ember.ArrayProxy.create({ content: Ember.A(buttons) });
     return Ember.A(buttons);
--- app/assets/javascripts/views/douche_button_view.js
+++ app/assets/javascripts/views/douche_button_view.js
@@ -1,18 +1,13 @@
 Wildcard.DoucheButtonView = Ember.View.extend({
   templateName: 'dbv',
-  // tagName: 'button',
-  url: function () {
-    return this.get('content.url');
-  }.observes('content'),
   didInsertElement: function () {
-    console.log(this);
   },
   click: function (evt) {
     var self = this;
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(function (position) {
         var coords = position.coords;
-        var tag = self.$().get(0).innerText;
+        var tag = self.$('a').attr('title');
         var card = Wildcard.Card.createRecord({
           tag: tag,
           location: {lat: coords.latitude, lng: coords.longitude}
