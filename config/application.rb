5a71187d44a01ca934da6205e8edd3c5815d742b
Hana Wang <h.wang081@gmail.com>
2013-05-18 10:11:01 -0500

    fix assets?

--- config/application.rb
+++ config/application.rb
@@ -61,6 +61,7 @@ module Wildcard
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    config.assets.paths << Rails.root.join("app", "assets", "stylesheets", "vendor")
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
