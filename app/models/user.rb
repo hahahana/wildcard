45cb7b1ad0e3c61629b5a643d0a32da0abf589a5
Hana Wang <h.wang081@gmail.com>
2013-05-18 14:25:27 -0500

    fixing user

--- app/models/user.rb
+++ app/models/user.rb
@@ -55,7 +55,6 @@ class User
       user.uid = auth.uid
       user.email = auth.info.email
       user.name = auth.info.name
-      user.profile_picture = auth.info.image.gsub("square", "large")
       user.oauth_token = auth.credentials.token
       user.oauth_expires_at = Time.at(auth.credentials.expires_at)
       user.save!
